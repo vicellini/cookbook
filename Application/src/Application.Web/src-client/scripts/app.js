d682134c0da42e377ff15390d62aab56f4b9daf8
Vince Cellini <vincecellini@Vinces-MacBook-Pro.local>
2017-03-14 18:07:45 -0400

    initial commit

--- Application/src/Application.Web/src-client/scripts/app.js
+++ Application/src/Application.Web/src-client/scripts/app.js
@@ -6,7 +6,7 @@ const SomeComponent = React.createClass({
 	render: function(){
 		return (
 			<div>
-				<h1> I &#10084; u<br/> Baby Shampu</h1>
+				<h1> WHOOAAAA!</h1>
 				<p><small>
 					you make my hair so soft and i know you will never make me cry.
 				</small></p>
