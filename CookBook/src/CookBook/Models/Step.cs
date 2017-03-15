716d417718588a27fa078c0ac283f8d0a12394dc
Jeff Heffner <jeffheffner@gmail.com>
2017-03-15 16:11:01 -0400

    Add files via upload
    you can now use a JSON object to create users accounts:
    {
         "Email":   "jeffheffner@gmail.com",
          "Password": "P@ssword1"
          
    
    }
    or, add a recipe:
    
    {
         "name":   "Latin Chicken and Rice",
          "category": "dinner",
          
          "ingredients" : [
              { "name" : "rice", "detail" : "1 cup" },
              { "name" : "chicken stock", "detail" : "2 cups" }
            ],
          "steps" : [
               { "description" : "put cat in oven"}
           ]
    }

---
+++ CookBook/src/CookBook/Models/Step.cs
@@ -0,0 +1,21 @@
+﻿using CookBook.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookBook.Models
+{
+    public class Step
+    {
+        public int Id { get; set; }
+        public string Duration { get; set; }
+        public string Description { get; set; }
+        public Recipe Recipe { get; set; }
+     
+        public Step()
+        {
+
+        }
+    }
+}
