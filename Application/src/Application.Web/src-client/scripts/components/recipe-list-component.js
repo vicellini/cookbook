6442d08997c81b56390731f326ad22cc748c7ec1
Will Dooley <dooley@Wills-MacBook-Air.local>
2017-03-21 14:32:01 -0400

    workin on recipeList

--- Application/src/Application.Web/src-client/scripts/components/recipe-list-component.js
+++ Application/src/Application.Web/src-client/scripts/components/recipe-list-component.js
@@ -1,43 +1,47 @@
 import React from 'react'
 
 export const RecipeListComponent = React.createClass({
+ getInitialState: function (){
+   return {}
+ },
 
   _makeRecipeComponents: function(recipeList){
     let arrayofRecipeComponents = recipeList.map(function(recipeObj, i){
       return (
-          <recipeData={recipeObj}
-          key={i}/>
+          <RecipeItem recipeObj={recipeObj} key={i}/>
       )
     })
 
     return arrayofRecipeComponents
+
   },
 
-  _filterRecipeByCategory: function(recipeList, mealType){
-    let filteredList = recipeList.filter(function(recipeObj){
-
-      if(mealType === recipeObj.category || mealType === "ALL"){
-        return true
-      } else {
-        return false
-      }
-    })
-    return filteredList
-  },
+  // _filterRecipeByCategory: function(recipeList, mealType){
+  //   let filteredList = recipeList.filter(function(recipeObj){
+  //
+  //     if(mealType === recipeObj.category || mealType === "ALL"){
+  //       return true
+  //     } else {
+  //       return false
+  //     }
+  //   })
+  //   return filteredList
+  // },
 
   render: function(){
     let self = this
+    console.log(this.props)
     let allTheRecipes = this.props.recipeList
-    let filteredRecipes = this._filterRecipeByCategory( allTheRecipes, this.props.shownMealType)
+    // let filteredRecipes = this._filterRecipeByCategory( allTheRecipes, this.props.shownMealType)
 
     return (
       <div className="cookBook-recipes">
         <h2>Recipes</h2>
-        {this._makeRecipeComponents(filteredRecipes)}
+        {/* {this._makeRecipeComponents(filteredRecipes)} */}
       </div>
     )
-  })
-
+  }
+})
   export const RecipeItem = React.createClass({
     render: function(){
       return (
