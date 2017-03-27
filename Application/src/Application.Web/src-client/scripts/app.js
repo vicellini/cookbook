b284a739758a060c32261d5e16a9c3bdf4d28d92
Jeff Heffner <jeffheffner@gmail.com>
2017-03-27 10:23:24 -0400

    Travis fixed line 6 so that I can now render styles!

--- Application/src/Application.Web/src-client/scripts/app.js
+++ Application/src/Application.Web/src-client/scripts/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import {ViewController} from './view-controller.js'
 
-if(window.location.hostname === 'localhost'){
+if(window.location.hostname === 'localhost' && window.location.port === "5000" ){
     let headEl = document.querySelector('head')
     let linkEl = document.querySelector('link[href="./css/styles.css"]')
     headEl.removeChild(linkEl)
